300038e28fb2ff886549af800445ec72d3e9f62a
madhu179 <madhubabu232343@gmail.com>
2020-10-07 21:07:31 +0530

    Commiting Use Case 3 Files

--- src/com/capgemini/linkedlist/LinkedListTest.java
+++ src/com/capgemini/linkedlist/LinkedListTest.java
@@ -17,9 +17,25 @@ public class LinkedListTest {
 		myLinkedList.addNode(secondNode);
 		myLinkedList.addNode(thirdNode);
 		myLinkedList.printNodes();
-		boolean result = myLinkedList.head.equals(thirdNode) &&
-				myLinkedList.head.getNext().equals(secondNode)&& 
-				myLinkedList.tail.equals(firstNode);
+		boolean result = myLinkedList.head.equals(thirdNode) && myLinkedList.head.getNext().equals(secondNode)
+				&& myLinkedList.tail.equals(firstNode);
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void given3NodesApeendingShouldPassTest() {
+
+		Node<Integer> firstNode = new Node<>(56);
+		Node<Integer> secondNode = new Node<>(30);
+		Node<Integer> thirdNode = new Node<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+
+		myLinkedList.appendNode(firstNode);
+		myLinkedList.appendNode(secondNode);
+		myLinkedList.appendNode(thirdNode);
+		myLinkedList.printNodes();
+		boolean result = myLinkedList.head.equals(firstNode) && myLinkedList.head.getNext().equals(secondNode)
+				&& myLinkedList.tail.equals(thirdNode);
 		Assert.assertTrue(result);
 	}
 
--- src/com/capgemini/linkedlist/MyLinkedList.java
+++ src/com/capgemini/linkedlist/MyLinkedList.java
@@ -1,44 +1,53 @@
 package com.capgemini.linkedlist;
 
 public class MyLinkedList {
-	
+
 	public INode head;
 	public INode tail;
-	
-	public MyLinkedList()
-	{
+
+	public MyLinkedList() {
 		this.head = null;
 		this.tail = null;
 	}
-	
-	public void addNode(INode newNode)
-	{
-		if(tail == null)
+
+	public void addNode(INode newNode) {
+		if (tail == null)
 			this.tail = newNode;
-		
-		if(head == null)
+
+		if (head == null)
 			this.head = newNode;
-		else
-		{
+		else {
 			INode tempNode = this.head;
 			newNode.setNext(tempNode);
 			this.head = newNode;
 		}
 	}
 
+	public void appendNode(INode newNode) {
+
+		if (head == null)
+			this.head = newNode;
+		if (tail == null)
+			this.tail = newNode;
+		else {
+			INode tempNode = this.tail;
+			tempNode.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
 	public void printNodes() {
 		INode tempNode = this.head;
 		String nodes = " ";
-		while(tempNode.getNext()!= null)
-		{
-			nodes = nodes+tempNode.getKey();
-			if(!tempNode.equals(tail))
-				nodes = nodes+"->";
+		while (tempNode.getNext() != null) {
+			nodes = nodes + tempNode.getKey();
+			if (!tempNode.equals(tail))
+				nodes = nodes + "->";
 			tempNode = tempNode.getNext();
 		}
-		nodes = nodes+tempNode.getKey();
-		System.out.println("Linked List : "+nodes);
-		
+		nodes = nodes + tempNode.getKey();
+		System.out.println("Linked List : " + nodes);
+
 	}
 
 }
